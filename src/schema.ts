62b108f4387c4dbb192afc82aa0eea43cfe787a0
hanwool kim <alwaysfun2183@gmail.com>
2020-04-09 20:04:49 +0900

    DateTime scalar 추가

--- src/schema.ts
+++ src/schema.ts
@@ -1,12 +1,13 @@
 import { makeExecutableSchema } from "graphql-tools";
 import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
+import { typeDefs, resolvers } from "graphql-scalars";
 import * as path from "path";
 
 const allTypes = fileLoader(path.join(__dirname, "/gql/**/*.gql"));
 const allResolvers = fileLoader(path.join(__dirname, "/gql/**/*.ts"));
 const schema = makeExecutableSchema({
-  typeDefs: mergeTypes(allTypes),
-  resolvers: mergeResolvers(allResolvers)
+  typeDefs: [...typeDefs, mergeTypes(allTypes)],
+  resolvers: { ...resolvers, ...mergeResolvers(allResolvers) }
 });
 
 export default schema;
