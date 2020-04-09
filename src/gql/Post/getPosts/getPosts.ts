dd3f4684d5b0bac6440d710178ff36d9d39e2e54
hanwool kim <alwaysfun2183@gmail.com>
2020-04-09 21:40:00 +0900

    after, offset 추가하여 pagenation

--- src/gql/Post/getPosts/getPosts.ts
+++ src/gql/Post/getPosts/getPosts.ts
@@ -3,8 +3,10 @@ import { IResolvers } from "graphql-tools";
 
 const query: IResolvers = {
   Query: {
-    getPosts: async (_, { boardId }, ctx, info) => {
+    getPosts: async (_, { boardId, after, offset: first = 10 }, ctx, info) => {
       return prisma.posts({
+        first,
+        after,
         where: {
           board: {
             id: boardId
