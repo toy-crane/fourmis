364d2bfac62b1cac033c47404e45b5b584176d5d
hanwool kim <alwaysfun2183@gmail.com>
2020-04-16 22:19:58 +0900

    fragment 삭제

--- src/gql/Board/createBoard/createBoard.ts
+++ src/gql/Board/createBoard/createBoard.ts
@@ -1,19 +1,16 @@
 import { prisma } from "../../../../generated/prisma-client";
 import { IResolvers } from "graphql-tools";
-import { BOARD_FRAGMENT } from "../../../fragment";
 
 const mutation: IResolvers = {
   Mutation: {
     createBoard: async (_, { productId }, ctx, info) => {
-      return await prisma
-        .createBoard({
-          product: {
-            connect: {
-              id: productId
-            }
+      return await prisma.createBoard({
+        product: {
+          connect: {
+            id: productId
           }
-        })
-        .$fragment(BOARD_FRAGMENT);
+        }
+      });
     }
   }
 };
