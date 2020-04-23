6172a17822bbae1f855d971ed2933ec1a4939ba1
hanwool kim <alwaysfun2183@gmail.com>
2020-04-23 20:13:01 +0900

    Post prisma2로 변경

--- src/gql/Post/createPost/createPost.ts
+++ src/gql/Post/createPost/createPost.ts
@@ -1,18 +1,20 @@
-import { prisma } from "../../../../generated/prisma-client";
+import { prisma } from "../../../prismaClient";
 import { IResolvers } from "graphql-tools";
 
 const mutation: IResolvers = {
   Mutation: {
     createPost: async (_, { boardId, content, title }, { user }) => {
-      return prisma.createPost({
-        board: {
-          connect: {
-            id: boardId
-          }
-        },
-        user: { connect: { id: user.id } },
-        title,
-        content
+      return prisma.post.create({
+        data: {
+          board: {
+            connect: {
+              id: boardId
+            }
+          },
+          user: { connect: { id: user.id } },
+          title,
+          content
+        }
       });
     }
   }
--- src/gql/Post/posts/posts.ts
+++ src/gql/Post/posts/posts.ts
@@ -1,16 +1,16 @@
 import { IResolvers } from "graphql-tools";
-import { prisma } from "../../../../generated/prisma-client";
+import { prisma } from "../../../prismaClient";
 
 const query: IResolvers = {
   PostsConnection: {
     edges: async ({ boardId, cursor, offset }) => {
-      const posts = await prisma.posts({
+      const posts = await prisma.post.findMany({
         where: {
           board: { id: boardId },
-          createdAt_lt: cursor
+          createdAt: { lt: cursor }
         },
         first: offset,
-        orderBy: "createdAt_DESC"
+        orderBy: { createdAt: "desc" }
       });
       const postEdges = posts.map(post => ({
         cursor: post.createdAt,
@@ -20,31 +20,28 @@ const query: IResolvers = {
     },
     pageInfo: async parent => parent,
     totalCount: async ({ boardId }) => {
-      return await prisma
-        .postsConnection({
-          where: {
-            board: {
-              id: boardId
-            }
-          }
-        })
-        .aggregate()
-        .count();
+      return await prisma.post.count({
+        where: {
+          board: { id: boardId }
+        }
+      });
     }
   },
   PostPageInfo: {
     startCursor: async ({ cursor, boardId }) => {
-      const postExists = await prisma.$exists.post({
-        board: { id: boardId },
-        createdAt_lt: cursor
+      const posts = await prisma.post.findMany({
+        where: {
+          board: { id: boardId },
+          createdAt: { lt: cursor }
+        }
       });
-      if (postExists) {
-        const posts = await prisma.posts({
+      if (posts.length > 0) {
+        const posts = await prisma.post.findMany({
           where: {
             board: { id: boardId },
-            createdAt_lt: cursor
+            createdAt: { lt: cursor }
           },
-          orderBy: "createdAt_DESC"
+          orderBy: { createdAt: "desc" }
         });
         return posts[0].createdAt;
       } else {
@@ -52,13 +49,13 @@ const query: IResolvers = {
       }
     },
     endCursor: async ({ cursor, boardId, offset }) => {
-      const posts = await prisma.posts({
+      const posts = await prisma.post.findMany({
         where: {
           board: { id: boardId },
-          createdAt_lt: cursor
+          createdAt: { lt: cursor }
         },
         first: offset,
-        orderBy: "createdAt_DESC"
+        orderBy: { createdAt: "desc" }
       });
       if (posts.length > 0) {
         return posts[posts.length - 1].createdAt;
@@ -68,22 +65,22 @@ const query: IResolvers = {
     },
     hasNextPage: async ({ cursor, boardId, offset }) => {
       return (
-        (await prisma
-          .postsConnection({
-            where: {
-              createdAt_lt: cursor,
-              board: { id: boardId }
-            }
-          })
-          .aggregate()
-          .count()) > offset
+        (await prisma.post.count({
+          where: {
+            createdAt: { lt: cursor },
+            board: { id: boardId }
+          }
+        })) > offset
       );
     },
     hasPreviousPage: async ({ boardId, cursor }) => {
-      return await prisma.$exists.post({
-        createdAt_gte: cursor,
-        board: { id: boardId }
+      const posts = await prisma.post.findMany({
+        where: {
+          board: { id: boardId },
+          createdAt: { gte: cursor }
+        }
       });
+      return posts.length > 0;
     }
   }
 };
