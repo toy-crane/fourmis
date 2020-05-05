d8929cd51cd4654db702f8207c77a889bcb81241
toycrane <toycrane@toycraneui-MacBookPro.local>
2020-05-05 19:28:25 +0900

    comment에 user 추가

--- src/gql/Comment/comment/comment.ts
+++ src/gql/Comment/comment/comment.ts
@@ -6,7 +6,7 @@ const query: IResolvers = {
     CommentlikesCount: async ({ id }, _, ctx: Context) => {
       const { user, prisma } = ctx;
       return await prisma.commentLike.count({
-        where: { comment: { id }, user: { id: user.id } }
+        where: { comment: { id }, user: { id: user.id } },
       });
     },
     isCommentLiked: async ({ id }, _, ctx: Context) => {
@@ -14,16 +14,27 @@ const query: IResolvers = {
       if (user) {
         const filterOptions = {
           comment: { id },
-          user: { id: user.id }
+          user: { id: user.id },
         };
         const commentLikes = await prisma.commentLike.findMany({
-          where: filterOptions
+          where: filterOptions,
         });
         return commentLikes.length > 0 ? true : false;
       } else {
         return false;
       }
-    }
-  }
+    },
+    user: async ({ id }, __, ctx: Context) => {
+      const { prisma } = ctx;
+      const users = await prisma.user.findMany({
+        where: {
+          comments: {
+            every: { id },
+          },
+        },
+      });
+      return users[0];
+    },
+  },
 };
 export default query;
